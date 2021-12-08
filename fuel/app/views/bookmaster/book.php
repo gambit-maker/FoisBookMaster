3906d304eabccbbf738fb01298fce22f9ebf07c0
DESKTOP-BMGVG31\quang <i2020-nguyendoquanghuy.nhatrang@fois-vn.com>
2021-12-08 12:02:29 +0700

    hoàn thành thiết kế giao diện

--- fuel/app/views/bookmaster/book.php
+++ fuel/app/views/bookmaster/book.php
@@ -6,12 +6,119 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+
+    use Fuel\Core\Form;
+
+    echo Asset::css('bootstrap.min.css');
+
+    ?>
+
 </head>
 
 <body>
-    <div>
+    <div style="margin: auto; width: 500px;" class="pt-5">
+        <h4>
+            Book maintence master
+            <a href="" style="float: right;">Đóng</a>
+        </h4>
+        <?php echo Form::open(['action' => 'book/add', 'method' => 'post']); ?>
 
+        <!-- Book ID + Tra cứu -->
+        <div>
+            <?php
+            echo Form::label('Book ID :', 'id');
+            echo Form::button('tracuu_btn', 'Tra cứu', ['class' => 'btn btn-primary', 'style' => 'float: right; margin-top:30px']);
+            ?>
+            <div style="overflow: hidden; padding-right: .5rem">
+                <?php
+                echo Form::input('id', '', ['class' => 'form-control', 'style' => 'width:80%']);
+
+                ?>
+            </div>
+        </div>
+
+        <!-- Book title -->
+
+        <div style="padding-top: 10px;">
+            <?php
+            echo Form::label('Book title :', 'title');
+            echo Form::input('title', '', ['class' => 'form-control']);
+            ?>
+        </div>
+
+        <!-- Tên tác giã -->
+        <div style="padding-top: 10px">
+            <?php
+            echo Form::label('Tên tác giã :', 'author');
+            echo Form::input('author', '', ['class' => 'form-control']);
+            ?>
+        </div>
+
+        <!-- Nhà xuất bản -->
+        <div style="padding-top: 10px;">
+            <?php
+            echo Form::label('Nhà xuất bản :', 'producer');
+            echo Form::input('producer', '', ['class' => 'form-control']);
+            ?>
+        </div>
+
+        <!-- thời gian xuất bản -->
+        <div style="padding-top: 10px">
+            <?php
+            echo Form::label('Ngày xuất bản: ');
+            ?>
+
+            <div class="d-flex flex-row">
+                <div style="width: 25%;">
+                    <?php
+                    echo Form::input('year', '', ['class' => 'form-control', 'style' => 'text-align:center;']);
+                    ?>
+                </div>
+                <div class="pt-2">
+                    <?php echo Form::label('Năm', 'year'); ?>
+                </div>
+
+                <div style="width: 10%;">
+                    <?php
+                    echo Form::input('month', '', ['class' => 'form-control', 'style' => 'text-align:center;']);
+                    ?>
+                </div>
+                <div class="pt-2">
+                    <?php echo Form::label('Tháng', 'month'); ?>
+                </div>
+
+                <div style="width: 10%;">
+                    <?php
+                    echo Form::input('date', '', ['class' => 'form-control', 'style' => 'text-align:center;']);
+                    ?>
+                </div>
+                <div class="pt-2">
+                    <?php echo Form::label('Ngày', 'date'); ?>
+                </div>
+            </div>
+
+            <div class="d-flex flex-row pt-3 float-right">
+                <div class="pl-5">
+                    <?php echo Form::button('them_btn', 'Thêm', ['class' => 'btn btn-primary', 'style' => 'width: 80px']) ?>
+                </div>
+                <div class="pl-5">
+                    <?php echo Form::button('update_btn', 'Update', ['class' => 'btn btn-warning', 'style' => 'width: 80px']) ?>
+                </div>
+                <div class="pl-5">
+                    <?php echo Form::button('xoa_btn', 'Xóa', ['class' => 'btn btn-danger', 'style' => 'width: 80px']) ?>
+                </div>
+                <div class="pl-5">
+                    <?php echo Form::button('clear_btn', 'Clear', ['class' => 'btn btn-secondary', 'style' => 'width: 80px']) ?>
+                </div>
+            </div>
+        </div>
+        <!-- button thêm xóa sửa -->
+
+        <?php echo Form::close(); ?>
     </div>
 </body>
+
+
 
 </html>
